0ffe8ec6215baa67dcd038aff9574cc8df8804af
Hana Xu <hana.xu@temple.edu>
2017-06-27 08:18:38 -0400

    added python link comments

--- src/widget_style.lisp
+++ src/widget_style.lisp
@@ -1,5 +1,6 @@
 (in-package :cl-jupyter-widgets)
 
+;;https://github.com/drmeister/widget-dev/blob/master/ipywidgets6/widgets/widget_style.py#L10
 (defclass style (widget)
   ()
   (:default-initargs
