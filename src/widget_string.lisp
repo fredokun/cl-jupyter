2b82306865eae21e2edafb84af46dee30a1ec3d2
Hana Xu <hana.xu@temple.edu>
2017-06-27 06:59:05 -0400

    updated for ipywidgets6

--- src/widget_string.lisp
+++ src/widget_string.lisp
@@ -1,25 +1,5 @@
 (in-package :cl-jupyter-widgets)
 
-#||
-class _String(DOMWidget):
-    """Base class used to create widgets that represent a string."""
-
-    _model_module = Unicode('jupyter-js-widgets').tag(sync=True)
-    _view_module = Unicode('jupyter-js-widgets').tag(sync=True)
-
-    value = Unicode(help="String value").tag(sync=True)
-    disabled = Bool(False, help="Enable or disable user changes").tag(sync=True)
-    description = Unicode(help="Description of the value this widget represents").tag(sync=True)
-    placeholder = Unicode("", help="Placeholder text to display when nothing has been typed").tag(sync=True)
-
-    def __init__(self, value=None, **kwargs):
-        if value is not None:
-            kwargs['value'] = value
-        super(_String, self).__init__(**kwargs)
-
-    _model_name = Unicode('StringModel').tag(sync=True)
-||#
-
 (defclass %string (labeled-widget value-widget core-widget)
   ((value :initarg :value :accessor value
 	   :type unicode
@@ -52,14 +32,6 @@ class _String(DOMWidget):
      :view-module (unicode "jupyter-js-widgets")
    )
    (:metaclass traitlets:traitlet-class))
-
-#||
-@register('Jupyter.HTML')
-class HTML(_String):
-    """Renders the string `value` as HTML."""
-    _view_name = Unicode('HTMLView').tag(sync=True)
-_model_name = Unicode('HTMLModel').tag(sync=True)
-||#
   
 (defclass html(%string)
   ()
@@ -70,15 +42,6 @@ _model_name = Unicode('HTMLModel').tag(sync=True)
   (:metaclass traitlets:traitlet-class))
 
 
-#||@register('Jupyter.Label')
-class Label(_String):
-    """Label widget.
-    It also renders math inside the string `value` as Latex (requires $ $ or
-    $$ $$ and similar latex tags).
-    """
-_view_name = Unicode('LabelView').tag(sync=True)
-||#
-
 (defclass label(%string)
   ()
   (:default-initargs
@@ -86,18 +49,6 @@ _view_name = Unicode('LabelView').tag(sync=True)
     :model-name (unicode "LabelModel")
     )
   (:metaclass traitlets:traitlet-class))
-
-
-#||
-@register('Jupyter.Textarea')
-class Textarea(_String):
-    """Multiline text area widget."""
-    _view_name = Unicode('TextareaView').tag(sync=True)
-    _model_name = Unicode('TextareaModel').tag(sync=True)
-
-    def scroll_to_bottom(self):
-self.send({"method": "scroll_to_bottom"})
-||#
 
 (defclass textarea(%string)
   ()
@@ -108,40 +59,9 @@ self.send({"method": "scroll_to_bottom"})
   (:metaclass traitlets:traitlet-class))
 
 
-#||
-@register('Jupyter.Text')
-class Text(_String):
-    """Single line textbox widget."""
-    _view_name = Unicode('TextView').tag(sync=True)
-    _model_name = Unicode('TextModel').tag(sync=True)
-
-    def __init__(self, *args, **kwargs):
-        super(Text, self).__init__(*args, **kwargs)
-        self._submission_callbacks = CallbackDispatcher()
-        self.on_msg(self._handle_string_msg)
-
-    def _handle_string_msg(self, _, content, buffers):
-        """Handle a msg from the front-end.
-        Parameters
-        ----------
-        content: dict
-            Content of the msg.
-        """
-        if content.get('event', '') == 'submit':
-            self._submission_callbacks(self)
-
-    def on_submit(self, callback, remove=False):
-        """(Un)Register a callback to handle text submission.
-        Triggered when the user clicks enter.
-        Parameters
-        ----------
-        callback: callable
-            Will be called with exactly one argument: the Widget instance
-        remove: bool (optional)
-            Whether to unregister the callback
-        """
-self._submission_callbacks.register_callback(callback, remove=remove)
-||#
+;;are these methods for text class needed?
+;;How would we go about translating these?
+;;https://github.com/drmeister/widget-dev/blob/master/ipywidgets6/widgets/widget_string.py#L81
 
 (defclass text(%string)
   ()
