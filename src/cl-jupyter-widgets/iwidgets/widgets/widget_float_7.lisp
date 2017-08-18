da13c7b55d1d4093781ce27f8c02c02e3d1a1723
Kevin Esslinger <tug74186@temple.edu>
2017-08-18 17:22:37 -0400

    Taking off style... for now

--- src/cl-jupyter-widgets/iwidgets/widgets/widget_float_7.lisp
+++ src/cl-jupyter-widgets/iwidgets/widgets/widget_float_7.lisp
@@ -145,12 +145,12 @@
 	  :metadata (:sync t
 			   :json-name "step"
 			   :help "Minimum step to increment the value"))
-   (style :initarg :style :accessor style
+ #|  (style :initarg :style :accessor style
 	  :initform (make-instance 'instance-dict :instance (make-instance 'slider-style))
 	  :metadata (:sync t
 			   :json-name "style"
 			   :from-json json-to-widget
-			   :to-json widget-to-json))
+			   :to-json widget-to-json))|#
    )
   (:default-initargs
    :view-name (unicode "FloatSliderView")
@@ -172,12 +172,12 @@
 	       :metadata (:sync t
 				:json-name "bar_style"
 				:help "Use a predefined styling for the progress bar. Options: 'success', 'info', 'warning', 'danger'"))
-   (style :initarg :style :accessor style
+  #| (style :initarg :style :accessor style
 	  :initform (make-instance 'instance-dict :instance (make-instance 'progress-style))
 	  :metadata (:sync t
 			   :json-name "style"
 			   :from-json json-to-widget
-			   :to-json widget-to-json))
+			   :to-json widget-to-json))|#
    )
   (:default-initargs
    :view-name (unicode "ProgressView")
@@ -272,12 +272,12 @@
 	     :metadata (:sync t
 			      :json-name "disabled"
 			      :help "Enable or disabled user changes"))
-   (style :initarg :style :accessor style
+  #| (style :initarg :style :accessor style
 	  :initform (make-instance 'instance-dict :instance (make-instance 'slider-style))
 	  :metadata (:sync t
 			   :json-name "style"
 			   :from-json json-to-widget
-			   :to-json widget-to-json))
+			   :to-json widget-to-json))|#
    )
   (:default-initargs
    :view-name (unicode "FloatRangeSliderView")
