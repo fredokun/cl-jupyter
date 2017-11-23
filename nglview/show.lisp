d04422464592ed55fc5a62d04214a23e0081a42b
drmeister <chris.schaf@verizon.net>
2017-11-23 18:30:53 -0500

    Write a message to log for show-pdbid

--- nglview/show.lisp
+++ nglview/show.lisp
@@ -8,7 +8,8 @@
 (defun show-pdbid (pdbid &rest kwargs &key &allow-other-keys)
   "Show PDB entry.
     Examples - (defparameter w (nglv:show_pdbid \"3pqr\"))
-              w"
+               (cl-jupyter-widgets::disp w)???????"
+  (cljw:widget-log "show-pdbid ~s~%" pdbid)
   (let ((structure (make-instance 'PdbIdStructure :pdbid pdbid)))
        (apply #'make-nglwidget :structure structure kwargs)))
 
